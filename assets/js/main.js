55da404467eef362989c8fbfb7ce00dd4e7730f9
Arnoldo Bezerra Pinheiro <arnoldobepi@gmail.com>
2023-07-11 21:46:21 -0300

    validar senha e usuario

--- assets/js/main.js
+++ assets/js/main.js
@@ -13,6 +13,27 @@ class ValidaFormulario {
     handleSubmit(e) {
         e.preventDefault();
         const camposValidos = this.isValid();
+        const senhasValidas = this.senhaIsValid();
+    }
+
+    senhaIsValid() {
+        let valid = true;
+
+        const senha = this.formulario.querySelector('.senha');
+        const repetirSenha = this.formulario.querySelector('.repetir-senha');
+
+        if(senha.value !== repetirSenha.value) {
+            valid = false;
+            this.criaErro(senha, 'Campos senhas e repitir senha precisar ser iguais.');
+            this.criaErro(repetirSenha, 'Campos senhas e repitir senha precisar ser iguais.');
+        }
+
+        if(senha.value.length < 6 || senha.value.length > 12) {
+            valid = false;
+            this.criaErro(senha, 'Senha precisa ter entre 6 a 12 caracteres.');
+        }
+
+        return valid;
     }
 
     isValid() {
@@ -32,8 +53,31 @@ class ValidaFormulario {
             if(campo.classList.contains('cpf')) {
                 if(!this.validaCPF(campo)) valid = false;
             }
+
+            if(campo.classList.contains('usuario')) {
+                if(!this.validaUsuario(campo)) valid = false;
+            }
         }
+
+        return valid;
     }
+
+    validaUsuario(campo) {
+        const usuario = campo.value;
+        let valid = true;
+
+        if(usuario.length < 3 || usuario.length > 12) {
+            this.criaErro(campo, 'Usuario precisa ter entre 3 e 12 caracteres.');
+            valid = false;
+        }
+
+        if(!usuario.match(/[a-zA-Z0-9]+/g)) {
+            this.criaErro(campo, 'Nome de usuario precisar conter apenas letras e numero.');
+            valid = false;
+        }
+
+        return valid;
+    } 
 
     validaCPF(campo) {
         const cpf = new ValidaCPF(campo.value);
